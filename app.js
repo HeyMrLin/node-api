0468e6f8e7bb61e16cc1272f7d73675c2de453d5
Linxi <lx02321@163.com>
2020-03-06 16:51:49 +0800

    添加weather接口

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const static_ = require('koa-static');
 const bodyParser = require('koa-bodyparser')
 const path = require("path")
 const registerRouter  = require('./routes')
+const cors = require('koa2-cors');
 
 app.use(bodyParser())
 // app.use(router.routes(), router.allowedMethods());
 app.use(registerRouter())
-
 app.use(static_(
   path.join(__dirname, '/public')
-))
+  ))
+app.use(cors({
+  origin: '*',
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+  maxAge: 5,
+  credentials: true,
+  allowMethods: ['GET', 'POST', 'DELETE'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}))
 module.exports = app;
---
+++ routes/weather.js
@@ -0,0 +1,21 @@
+const Router = require('koa-router');
+const router = new Router();
+const koa2Req = require('koa2-request');
+router.prefix('/weather');
+
+
+router.get('/', async (ctx, next) => {
+  const ip_url = `http://apis.juhe.cn/ip/ipNew?ip=${ctx.request.ip}&key=545bd393ee4b2b4506fc242b06fb6e45`;
+  const address = await koa2Req(ip_url);
+  const city = address.result.City;
+  const url = `http://apis.juhe.cn/simpleWeather/query?city=${city}&key=1ff6e706692fbd074aa2ed98924614fc`
+  const w = await koa2Req(encodeURI(url));
+  ctx.response.body = w.body;
+  // next;
+});
+
+router.get('/bar', function *(next) {
+  this.body = 'this is a users/bar response!';
+});
+
+module.exports = router;
