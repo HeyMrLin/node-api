87273a9038b15cd0339346eafb99199d096ddfad
挖坑小能手 <helloworld@hey.local>
2020-03-08 19:13:05 +0800

    修改接口

--- routes/weather.js
+++ routes/weather.js
@@ -7,7 +7,7 @@ router.prefix('/weather');
 router.get('/', async (ctx, next) => {
   const ip_url = `http://apis.juhe.cn/ip/ipNew?ip=${ctx.request.ip}&key=545bd393ee4b2b4506fc242b06fb6e45`;
   const address = await koa2Req(ip_url);
-  const city = address.result.City;
+  const city = address.result? address.result.City : '深圳';
   const url = `http://apis.juhe.cn/simpleWeather/query?city=${city}&key=1ff6e706692fbd074aa2ed98924614fc`
   const w = await koa2Req(encodeURI(url));
   ctx.response.body = w.body;
