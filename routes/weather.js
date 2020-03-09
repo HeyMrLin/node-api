bbb0f2aeee861bff481b38935c41f918a619866b
Linxi <lx02321@163.com>
2020-03-09 10:14:30 +0800

    修改入口

--- routes/weather.js
+++ routes/weather.js
@@ -1,7 +1,7 @@
 const Router = require('koa-router');
 const router = new Router();
 const koa2Req = require('koa2-request');
-router.prefix('/weather');
+// router.prefix('/weather');
 
 
 router.get('/', async (ctx, next) => {
